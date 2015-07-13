fc36fd1d6aa0e514d87042b7ac7942db418c80a0
Haridarshan HS <hsdars@gmail.com>
2015-07-13 11:57:38 -0500

    income tracker module working

--- ResideMenuDemo/src/com/special/ResideMenuDemo/IncomeTrackerFragment.java
+++ ResideMenuDemo/src/com/special/ResideMenuDemo/IncomeTrackerFragment.java
@@ -1,11 +1,14 @@
 package com.special.ResideMenuDemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.vinsol.expensetracker.Home;
 
 /**
  * User: special
@@ -18,6 +21,8 @@ public class IncomeTrackerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Toast.makeText(getActivity(), "IncomeTrackerFragment", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getActivity(), Home.class);
+        startActivity(intent);
         return inflater.inflate(R.layout.incometracker, container, false);
     }
 
