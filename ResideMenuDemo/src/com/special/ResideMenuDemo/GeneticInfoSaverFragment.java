a792de7fd6420b2904107adda88e39dd3ed75f55
Haridarshan HS <hsdars@gmail.com>
2015-07-14 10:53:36 -0500

    fragments updated

--- ResideMenuDemo/src/com/special/ResideMenuDemo/GeneticInfoSaverFragment.java
+++ ResideMenuDemo/src/com/special/ResideMenuDemo/GeneticInfoSaverFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -14,11 +16,16 @@ import android.widget.Toast;
  * Mail: specialcyci@gmail.com
  */
 public class GeneticInfoSaverFragment extends Fragment {
-
+    WebView webView;
+    String summary;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Toast.makeText(getActivity(), "GeneticInfoSaverFragment", Toast.LENGTH_SHORT).show();
-        return inflater.inflate(R.layout.geneticinfosaver, container, false);
+        View view=inflater.inflate(R.layout.geneticinfosaver, container, false);;
+        summary = "<html><body><script type='text/javascript' src='https://www.healthtap.com/widget/docdir.js' ></script><div id='htapWidgetTopdoc' data-highlight='1,3,2' style='height: 540px; width: 500px;'> by</span></div></body></html>";
+        webView= (WebView) view.findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadData(summary, "text/html", null);
+        return view;
     }
 
 }
--- ResideMenuDemo/src/com/special/ResideMenuDemo/InsulinHelperFragment.java
+++ ResideMenuDemo/src/com/special/ResideMenuDemo/InsulinHelperFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -14,11 +16,16 @@ import android.widget.Toast;
  * Mail: specialcyci@gmail.com
  */
 public class InsulinHelperFragment extends Fragment {
-
+    WebView webView;
+    String summary;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Toast.makeText(getActivity(), "InsulinHelperFragment", Toast.LENGTH_SHORT).show();
-        return inflater.inflate(R.layout.insulinhelper, container, false);
+        View view=inflater.inflate(R.layout.insulinhelper, container, false);;
+        summary = "<html><body><script type='text/javascript' src='https://www.healthtap.com/widget/askdoc.js' ></script><div id='htapWidgetAskdoc' data-color='rgb(246, 246, 246)' style='height: 680px; width: 500px;'></div></body></html>";
+        webView= (WebView) view.findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadData(summary, "text/html", null);
+        return view;
     }
 
 }
