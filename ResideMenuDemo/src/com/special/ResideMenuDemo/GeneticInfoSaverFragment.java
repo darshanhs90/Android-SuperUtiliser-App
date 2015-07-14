f6ca07931b4fb5884e75ca17010a50820a750d89
Haridarshan HS <hsdars@gmail.com>
2015-07-14 17:56:48 -0500

    home layout changed

--- ResideMenuDemo/src/com/special/ResideMenuDemo/GeneticInfoSaverFragment.java
+++ ResideMenuDemo/src/com/special/ResideMenuDemo/GeneticInfoSaverFragment.java
@@ -21,7 +21,7 @@ public class GeneticInfoSaverFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.geneticinfosaver, container, false);;
-        summary = "<html><body><script type='text/javascript' src='https://www.healthtap.com/widget/docdir.js' ></script><div id='htapWidgetTopdoc' data-highlight='1,3,2' style='height: 540px; width: 500px;'> by</span></div></body></html>";
+        summary = "<html><body><script type='text/javascript' src='https://www.healthtap.com/widget/docdir.js' ></script><div id='htapWidgetTopdoc' data-highlight='1,3,2' style='height: 540px; width: 500px;'></div></body></html>";
         webView= (WebView) view.findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadData(summary, "text/html", null);
